7ba29e52e8d96808c9d9aa757246c131193fee46
Lyes Saadi <mail@lyes.eu>
2019-06-01 17:07:24 +0100

    Phew... Now authorization forms work !

--- src/Controller/AuthorizationController.php
+++ src/Controller/AuthorizationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Yaml\Yaml;
@@ -15,45 +16,53 @@ class AuthorizationController extends Controller
      *     "_locale": "%app.locales%"
      * })
      */
-    public function manageAction(Request $request)
+    public function authorizationManageAction(Request $request)
     {
         $yaml = Yaml::parseFile(__dir__.'/../../config/authorizations.yaml');
         $forms = [];
         foreach ($yaml as $role => $authorizations) {
             if ($role != "__META__") {
                 foreach ($authorizations as $category => $values) {
-                    $forms[$role][$category] = $this->createFormBuilder($values);
                     foreach ($yaml["__META__"][$category] as $key => $options) {
                         if (!\array_key_exists($key, $values)) {
                             $values[$key] = $options["default"];
                         }
+                    }
+                    $forms[$role][$category] = $this->container->get('form.factory')->createNamedBuilder($role.'_'.$category, FormType::class, $values, ['action' => $this->generateUrl('authorization_manage_submit', ['role' => $role, 'category' => $category])]);
+                    foreach ($yaml["__META__"][$category] as $key => $options) {
                         $input_var = key_exists("input_var", $options) ? $options["input_var"] : [];
                         $forms[$role][$category] = $forms[$role][$category]->add($key, $options["input"], $input_var + ["required" => False]);
                     }
-                    $forms[$role][$category] = $forms[$role][$category]->add('send', SubmitType::class)->getForm();
+                    $forms[$role][$category] = $forms[$role][$category]->add('send', SubmitType::class)->getForm()->createView();
                 }
             }
         }
-        foreach ($forms as $role => $role_value) {
-            foreach ($role_value as $category => $cat_value) {/*
-                if ($cat_value->isSubmitted() && $cat_value->isValid()) {
-                    $yaml[$role][$category] = $cat_value->getData();
-                }*/
-                $forms[$role][$category] = $cat_value->createView();
-            }
-        }
 
-        /*
-        $yaml = Yaml::dump($yaml, 99, 4);
-        file_put_contents(__dir__.'/../../config/authorizations.yaml', $yaml);
-        */
         return $this->render('authorization/manage.html.twig', [
             'forms' => $forms,
         ]);
     }
 
-    public function manageSubmitAction(Request $request)
+    /**
+     * @Route("/{_locale}/admin/manage_authorization/submit/{role}/{category}", name="authorization_manage_submit", requirements={
+     *     "_locale": "%app.locales%"
+     * })
+     */
+    public function authorizationManageSubmitAction(Request $request, string $role, string $category)
     {
-        //
+        $yaml = Yaml::parseFile(__dir__.'/../../config/authorizations.yaml');
+        $form = $this->container->get('form.factory')->createNamedBuilder($role.'_'.$category, FormType::class, $yaml[$role][$category], ['action' => $this->generateUrl('authorization_manage_submit', ['role' => $role, 'category' => $category])]);
+        foreach ($yaml["__META__"][$category] as $key => $options) {
+            $input_var = key_exists("input_var", $options) ? $options["input_var"] : [];
+            $form = $form->add($key, $options["input"], $input_var + ["required" => False]);
+        }
+        $form = $form->add('send', SubmitType::class)->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $yaml[$role][$category] = $form->getData();
+        }
+        $yaml = Yaml::dump($yaml, 99, 4);
+        file_put_contents(__dir__.'/../../config/authorizations.yaml', $yaml);
+        return $this->redirectToRoute('security_admin');
     }
 }
